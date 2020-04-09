43c82a7a705a2f851ee7e6777a9332b95d9381a9
Canicatti Alessandro <ales.canicatti@gmail.com>
2020-04-09 12:37:39 +0200

    emacs: C-x b uses counsel-switch-buffer

--- unix/.emacs.d/init.el
+++ unix/.emacs.d/init.el
@@ -149,3 +149,5 @@
 (global-set-key (kbd "C-x 3") (lambda () (interactive)(split-window-right) (other-window 1)))
 
 (setq recenter-positions '(top middle bottom))
+
+(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
